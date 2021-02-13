62e891a4fe51f75a7c5104b9fa332e8adcde677d
Abhi Patel <pate0789@algonquinlive.com>
2021-02-13 00:03:56 -0500

    Adding Files with git ignore

---
+++ Lab3/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Lab3.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Lab3.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Razor()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Count()
+        {
+            HttpContext.Session.SetString("Bottle", Request.Form["Bottle"]);
+            // HttpContext.Session.SetString("password", Request.Form["password"]); // note you probably wouldn't normally save this value
+            // ViewBag.Bottle = Request.Form["Bottle"];
+            return View();
+        }
+
+        public IActionResult CreatePerson()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person model)
+        {
+
+            return View(model);
+        }
+
+    }
+}
---
+++ Lab3/Models/Person.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab3.Models
+{
+    public class Person
+    {
+        [Required]
+        [StringLength(255)]
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+
+        public string Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string EmailAddress
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(255)]
+        [DataType(DataType.Date)]
+        public string Dob
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(100)]
+        [BindProperty(Name = "pass")]
+        public string Password
+        {
+            get;
+            set;
+        }
+        [Required]
+        [StringLength(100)]
+
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
